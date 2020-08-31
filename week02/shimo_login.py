38fba1868ca0a90af23cf457cee28a558ff27ad9
zengkaimin <704571330@qq.com>
2020-08-31 23:26:42 +0800

    update

--- week02/shimo_login.py
+++ week02/shimo_login.py
@@ -7,24 +7,23 @@
 from selenium import webdriver
 from time import sleep
 
-class TestCase(object):
+class TestShimo(object):
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://shimo.im/login?from=home")
         self.driver.maximize_window()
         sleep(1)
 
-    def test_xpath(self):
+    def test_login(self):
         self.driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div/input').send_keys("15902038154")
         self.driver.find_element_by_xpath(
             '//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/input').send_keys("qazwsx")
         self.driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/button').click()
-        sleep(10)
+        sleep(8)
         self.driver.quit()
 
-
 if __name__ == "__main__":
-    case = TestCase()
-    case.test_xpath()
+    case = TestShimo()
+    case.test_login()
 
 
